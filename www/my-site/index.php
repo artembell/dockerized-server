9021592dd0d8884fb04203accb1057bfe114b733
Artem Belyavskiy <artembell305@gmail.com>
2020-04-02 21:15:34 +0300

    Refactor index.php and PathResolver

--- www/my-site/index.php
+++ www/my-site/index.php
@@ -5,21 +5,15 @@ use models\PathResolver;
 require_once __DIR__ . '/bootstrap.php';
 require_once 'models/PathResolver.php';
 
-$serverRoot = $_SERVER["DOCUMENT_ROOT"];
 $pathResolver = new PathResolver(
-    $serverRoot,
-    $serverPagesDir = "public/pages"
+    $serverRoot = $_SERVER['DOCUMENT_ROOT'],
+    $serverPagesDir = 'public/pages'
 );
 
-echo "<pre>";
-echo "</pre>";
-
-$targetPath = isset($_GET['urn']) ? $_GET['urn'] : PathResolver::$defaultPage;
-
+$targetPage = $_GET['urn'] ?? $pathResolver->$defaultPage;
 
 echo $twig->render('index.html', [
-    'links' => PathResolver::$paths,
-    'contentPage' => $targetPath,
+    'links' => $pathResolver->getLinks()
 ]);
 
-$page = require_once($pathResolver->resolvePath($targetPath));
+require_once($pathResolver->resolvePathTo($targetPage));
--- www/my-site/models/PathResolver.php
+++ www/my-site/models/PathResolver.php
@@ -4,57 +4,66 @@ namespace models;
 
 class PathResolver
 {
-    public static $defaultPage = 'home';
-
-    public static $paths = [
-        [
-            "text" => "Home",
-            "location" => "/home",
-        ],
-        [
-            "text" => "About",
-            "location" => "/about",
-        ],
-        [
-            "text" => "Support",
-            "location" => "/support",
-        ],
-        [
-            "text" => "Pricing",
-            "location" => "/pricing",
-        ],
-        [
-            "text" => "Contacts",
-            "location" => "/contacts",
-        ],
-        [
-            "text" => "Laba 2",
-            "location" => "/laba-2",
-        ],
-    ];
+    private const HOME_PAGE = 'home';
+    private const NOT_FOUND_PAGE = '404';
+    public $defaultPage = self::HOME_PAGE;
 
     private $serverRoot;
     private $serverPagesDir;
+    private $links;
 
-    public function __construct(string $serverRoot, string $serverPagesDir) {
+    public function __construct(string $serverRoot, string $serverPagesDir)
+    {
         $this->serverRoot = $serverRoot;
         $this->serverPagesDir = $serverPagesDir;
-    }  
+        $this->links = [
+            [
+                'text' => 'Home',
+                'location' => '/home',
+            ],
+            [
+                'text' => 'About',
+                'location' => '/about',
+            ],
+            [
+                'text' => 'Support',
+                'location' => '/support',
+            ],
+            [
+                'text' => 'Pricing',
+                'location' => '/pricing',
+            ],
+            [
+                'text' => 'Contacts',
+                'location' => '/contacts',
+            ],
+            [
+                'text' => 'Labwork #2',
+                'location' => '/labwork-2',
+            ],
+        ];
+    }
 
-    public function resolvePath(string $target): string {
-        $absolutePagesPath = $this->serverRoot."/".$this->serverPagesDir;
-        $absolutePath = $absolutePagesPath."/".$target.".php";
+    public function getLinks(): array
+    {
+        return $this->links;
+    }
+
+    public function resolvePathTo(string $target): string
+    {
+        $absolutePagesPath = $this->serverRoot.'/'.$this->serverPagesDir;
+        $absolutePath = $absolutePagesPath.'/'.$target.'.php';
         
         $finalTarget;
 
-        if ($target == "") {
-            $finalTarget = "home";
-        } else if (file_exists($absolutePath)) {
+        if ($target == '') {
+            $finalTarget = self::HOME_PAGE;
+        } elseif (file_exists($absolutePath)) {
             $finalTarget = $target;
         } else {
-            $finalTarget = "404";
+            $finalTarget = self::NOT_FOUND_PAGE;
         }
 
-        return $absolutePagesPath."/".$finalTarget.".php";;
+        return $absolutePagesPath.'/'.$finalTarget.'.php';
     }
 }
