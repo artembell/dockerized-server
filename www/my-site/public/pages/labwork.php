823eaa723c673d396797b5343437a00fc53ca88f
Artem Belyavskiy <artembell305@gmail.com>
2020-04-04 19:38:31 +0300

    Add task working script

--- www/my-site/public/pages/labwork.php
+++ www/my-site/public/pages/labwork.php
@@ -3,6 +3,34 @@
 
 <?php
 
-    
+use models\TextFormatter;
+
+function getOldValueByInputKey($inputKey): string
+{
+    return isset($_POST[$inputKey]) ? $_POST[$inputKey] : '';
+}
+?>
+    <form action='labwork' method='POST'>
+        <label for='user-text'>Text:</label>
+        <textarea name='user-text'
+            id='user-text'
+            cols='30'
+            rows='10'
+        ><?= getOldValueByInputKey('user-text') ?></textarea>
+
+        <input type='submit' value='Submit'/>
+    </form>
+<?php
+
+
+$text = isset($_POST['user-text']) ? $_POST['user-text'] : '';
+$formatter = new TextFormatter();
+
+echo '<h2>Text</h2>';
+echo $text;
+
+echo '<h2>Formatted text</h2>';
+echo $formatter->format($text);
+
 ?>
 </main>
