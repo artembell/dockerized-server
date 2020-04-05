8b3fd446c406202182ab7ffc3e01b17d366cc67e
Artem Belyavskiy <artembell305@gmail.com>
2020-04-05 18:25:49 +0300

    Change the way FormValidator defines needed validation function

--- www/my-site/models/FormValidator.php
+++ www/my-site/models/FormValidator.php
@@ -8,21 +8,51 @@ class FormValidator
     private $validationData;
     private $pathRoot;
 
+    private $validateFunctions;
+
     public function __construct(array $inputNamesToValidate, string $root)
     {
         $this->inputNamesToValidate = $inputNamesToValidate;
         $this->validationData = [];
         $this->pathRoot = $root;
+
+        $this->setValidateFunctions();
     }
 
-    public function validateInput($data): void
+    private function setValidateFunctions(): void
     {
-        foreach ($this->inputNamesToValidate as $value) {
-            $validateFunc = $this->resolveIntoValidateFunctionName($value);
+        $validateFolderPath = function (string $path): string {
+            if (empty(trim($path))) {
+                return 'Path to the folder should not be empty';
+            } elseif (preg_match('/\s+/', $path)) {
+                return 'Path should not contain whitespaces';
+            } elseif (!preg_match('/^\//', $path)) {
+                return 'Path should start with /';
+            } elseif (!file_exists($this->pathRoot.$path)) {
+                return 'Such folder does not exist';
+            } elseif (!is_dir($this->pathRoot.$path)) {
+                return 'Path points not to a folder';
+            } else {
+                return '';
+            }
+        };
 
-            $inputValue = empty($data[$value]) ? '' : $data[$value];
-            $this->validationData[$value] = [
-                'error-message' => $this->$validateFunc($inputValue)
+        $this->validateFunctions['path'] = $validateFolderPath;
+    }
+
+    private function getValidateFunction(string $inputName): callable
+    {
+        return $this->validateFunctions[$inputName];
+    }
+
+    public function validateInput(array $data): void
+    {
+        foreach ($this->inputNamesToValidate as $inputName) {
+            $validateFunc = $this->getValidateFunction($inputName);
+
+            $inputValue = empty($data[$inputName]) ? '' : $data[$inputName];
+            $this->validationData[$inputName] = [
+                'error-message' => $validateFunc($inputValue)
             ];
         }
     }
@@ -32,41 +62,23 @@ class FormValidator
         return $this->validationData[$inputKey]['error-message'];
     }
 
-    public function isCorrectInput(string $inputName): bool
+    public function formHasCorrectInput(): bool
+    {
+        foreach ($this->inputNamesToValidate as $inputName) {
+            if (!$this->fieldHasCorrectInput($inputName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private function fieldHasCorrectInput(string $inputName): bool
     {
         return empty($this->validationData[$inputName]['error-message']);
     }
 
-    public function getCorrectnessClass(string $inputKey): string
+    public function getCorrectnessClass(string $inputName): string
     {
-        return $this->isCorrectInput($inputKey) ? 'no-errors' : 'has-error';
-    }
-
-    private function validate(string $inputKey, $data): string
-    {
-        $validateFunctionName = resolveIntoValidateFunctionName($inputKey);
-        return $validateFunctionName($data);
-    }
-
-    private function resolveIntoValidateFunctionName(string $inputKey): string
-    {
-        return 'validate'.ucfirst($inputKey).'Input';
-    }
-
-    private function validatePathInput(string $path): string
-    {
-        if (empty(trim($path))) {
-            return 'Path to the folder should not be empty';
-        } elseif (preg_match('/\s+/', $path)) {
-            return 'Path should not contain whitespaces';
-        } elseif (!preg_match('/^\//', $path)) {
-            return 'Path should start with /';
-        } elseif (!file_exists($this->pathRoot.$path)) {
-            return 'Such folder does not exist';
-        } elseif (!is_dir($this->pathRoot.$path)) {
-            return 'Path points not to a folder';
-        } else {
-            return '';
-        }
+        return $this->fieldHasCorrectInput($inputName) ? 'no-errors' : 'has-error';
     }
 }
